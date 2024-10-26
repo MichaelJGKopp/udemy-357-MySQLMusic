1aa542a23a9cfc79b627a0d65c244bb635cf709f
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-27 00:04:25 +0200

    statement.execute(query) returns always true with SELECT

--- src/dev/lpa/MusicDML.java
+++ src/dev/lpa/MusicDML.java
@@ -9,12 +9,12 @@ public class MusicDML {
   
   public static void main(String[] args) {
     
-    // driver before jdbc 4.0, loaded explicitly
-    try {
-      Class.forName("com.mysql.jdbc.Driver"); // allows to switch drivers during runtime
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+//    // driver before jdbc 4.0, loaded explicitly
+//    try {
+//      Class.forName("com.mysql.jdbc.Driver"); // allows to switch drivers during runtime
+//    } catch (ClassNotFoundException e) {
+//      throw new RuntimeException(e);
+//    }
     
     try (Connection connection = DriverManager.getConnection(
       "jdbc:mysql://localhost:3306/music",
@@ -22,10 +22,10 @@ public class MusicDML {
       System.getenv("MYSQL_PASS"));
          Statement statement = connection.createStatement();
     ) {
-      String artist = "Elf";
+      String artist = "Neil Young"; // not int able
       String query = "SELECT * FROM artists WHERE artist_name ='%s'"
                        .formatted(artist);
-      boolean result = statement.execute(query);
+      boolean result = statement.execute(query);  // returns always true with SELECT
       System.out.println("result = " + result);
     } catch (SQLException e) {
       throw new RuntimeException(e);
